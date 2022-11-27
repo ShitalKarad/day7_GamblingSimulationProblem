78e805d36c74586f518f850de6fea85f1fb69601
ShitalKarad <shitalkarad24@gmail.com>
2022-11-27 23:08:48 +0530

    Added : UC1 gambling simulation problem start

---
+++ com/brigelabz/GamblingSimulationProblem.java
@@ -0,0 +1,10 @@
+package com.brigelabz;
+
+public class GamblingSimulationProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome in Gambling Simulation Problem");
+        int stake = 100;
+        int bet = 1;
+        System.out.println("start with a stake of " + stake +" every day and bet " + bet +" every game.");
+    }
+}
