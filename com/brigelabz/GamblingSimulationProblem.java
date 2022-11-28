418db7538b3e1192aa63ee7a804a2102859a7706
ShitalKarad <shitalkarad24@gmail.com>
2022-11-28 14:48:20 +0530

    Added : UC2_gambling win or loose

--- com/brigelabz/GamblingSimulationProblem.java
+++ com/brigelabz/GamblingSimulationProblem.java
@@ -1,10 +1,24 @@
 package com.brigelabz;
 
+import java.util.Random;
+
 public class GamblingSimulationProblem {
     public static void main(String[] args) {
         System.out.println("Welcome in Gambling Simulation Problem");
         int stake = 100;
         int bet = 1;
-        System.out.println("start with a stake of " + stake +" every day and bet " + bet +" every game.");
+        int count =0;
+        Random random = new Random();
+       int num = random.nextInt(2) + 0;
+
+        if (num == 1){
+            stake += bet;
+            System.out.println("gambler is win by "+ bet+" & total Stake is " + stake);
+
+        }
+        else if (num == 0){
+            stake -=bet;
+            System.out.println("gambler is loose by "+bet+" & total Stake is "+ stake );
+        }
     }
 }
